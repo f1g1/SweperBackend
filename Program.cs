e4a3d71e932eae0a3b297446676fde16bd1e88f0
george <filipoiugeorge98@gmail.com>
2022-04-10 22:37:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("127.0.0.0.1").AllowAnyMethod().AllowAnyHeader();
+}));
+
+using (StreamReader r = new StreamReader(@"C:\Users\George\source\repos\SweperBackend\Firebase\firebase.json"))
+{
+    string json = r.ReadToEnd();
+    FirebaseApp.Create(new AppOptions
+    {
+        Credential = GoogleCredential.FromJson(json)
+    }); ;
+}
+
+
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["Jwt:Firebase:ValidIssuer"];
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Firebase:ValidIssuer"],
+        ValidAudience = builder.Configuration["Jwt:Firebase:ValidAudience"]
+    };
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
